5c4c51b24ed9df5ffdfaaf46b3b1fc540dd82c98
Vitouliss14 <vitouliss14@yahoo.com>
2012-10-08 21:43:24 -0400

    Not even ready yet

---
+++ DanceFunctions.lua
@@ -0,0 +1,32 @@
+local h = script.Parent["Head"]
+local la = script.Parent["Left Arm"]
+local ll = script.Parent["Left Leg"]
+local ra = script.Parent["Right Arm"]
+local rl = script.Parent["Right Leg"]
+
+function Dance()
+	for a = 1,5 do
+		h.CFrame = h.CFrame*CFrame.Angles(0,-1,0)
+		wait(0.1)
+	end
+	for b = 1,5 do
+		h.CFrame = h.CFrame*CFrame.Angles(0,-1,0)
+		wait(0.1)
+	end
+end
+
+function Jump()
+
+end
+	
+
+function Slide()
+
+end
+
+function Poop()
+
+
+end 
+
+Dance()
---
+++ README.md
@@ -0,0 +1,5 @@
+# Dance Functions
+
+Also known as Animation functions that I wrote to help you make your made up characters move in funny ways. As mentioned these are mostly Dance functions. 
+
+Currently there is nothing here to show expect for some useless code. 
