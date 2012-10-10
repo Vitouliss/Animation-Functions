f8227b41a1b2e4ec3670af464efd195364e2f6d8
Vitouliss14 <vitouliss14@yahoo.com>
2012-10-10 16:22:39 -0300

    Still slowly adding code.

--- DanceFunctions.lua
+++ DanceFunctions.lua
@@ -6,21 +6,42 @@ local rl = script.Parent["Right Leg"]
 
 function Dance()
 	for a = 1,5 do
-		h.CFrame = h.CFrame*CFrame.Angles(0,-1,0)
+		h.CFrame = h.CFrame*CFrame.Angles(0,-0.2,0)
 		wait(0.1)
 	end
 	for b = 1,5 do
-		h.CFrame = h.CFrame*CFrame.Angles(0,-1,0)
+		h.CFrame = h.CFrame*CFrame.Angles(0,0.2,0)
 		wait(0.1)
 	end
 	for c = 1,5 do
-		la.CFrame = la.CFrame*CFrame.Angles(1,0,0)
-		ra.CFrame = ra.CFrame*CFrame.Angles(-1,0,0)
+		la.CFrame = la.CFrame*CFrame.Angles(0.2,0,0)
+		ra.CFrame = ra.CFrame*CFrame.Angles(-0.2,0,0)
 		wait(0.1)
 	end
 	for d = 1,5 do
-		la.CFrame = la.CFrame*CFrame.Angles(-1,0,0)
-		ra.CFrame = ra.CFrame*CFrame.Angles(1,0,0)
+		la.CFrame = la.CFrame*CFrame.Angles(-0.2,0,0)
+		ra.CFrame = ra.CFrame*CFrame.Angles(0.2,0,0)
+		wait(0.1)
+	end
+	--Time to put them all together.
+	for a = 1,5 do
+		h.CFrame = h.CFrame*CFrame.Angles(0,-0.2,0)
+		la.CFrame = la.CFrame*CFrame.Angles(0.2,0,0)
+		ra.CFrame = ra.CFrame*CFrame.Angles(-0.2,0,0)
+		wait(0.1)
+	end
+	for b = 1,5 do
+		h.CFrame = h.CFrame*CFrame.Angles(0,0.2,0)
+		la.CFrame = la.CFrame*CFrame.Angles(-0.2,0,0)
+		ra.CFrame = ra.CFrame*CFrame.Angles(0.2,0,0)
+		wait(0.1)
+	end
+	for headiscool = 1,6 do
+		h.CFrame = h.CFrame*CFrame.Angles(0,0.2,0)
+		wait(0.1)
+	end
+	for headiscool2 = 1,6 do
+		h.CFrame = h.CFrame*CFrame.Angles(0,-0.2,0)
 		wait(0.1)
 	end
 end
