7f8e0a5e6203bdd787bbd21b749acdfd7be15d06
Vitouliss14 <vitouliss14@yahoo.com>
2012-10-09 21:34:15 -0300

    SLOWLY adding more lines

--- DanceFunctions.lua
+++ DanceFunctions.lua
@@ -1,4 +1,4 @@
-local h = script.Parent["Head"]
+local h = script.Parent.Head
 local la = script.Parent["Left Arm"]
 local ll = script.Parent["Left Leg"]
 local ra = script.Parent["Right Arm"]
@@ -11,6 +11,16 @@ function Dance()
 	end
 	for b = 1,5 do
 		h.CFrame = h.CFrame*CFrame.Angles(0,-1,0)
+		wait(0.1)
+	end
+	for c = 1,5 do
+		la.CFrame = la.CFrame*CFrame.Angles(1,0,0)
+		ra.CFrame = ra.CFrame*CFrame.Angles(-1,0,0)
+		wait(0.1)
+	end
+	for d = 1,5 do
+		la.CFrame = la.CFrame*CFrame.Angles(-1,0,0)
+		ra.CFrame = ra.CFrame*CFrame.Angles(1,0,0)
 		wait(0.1)
 	end
 end
